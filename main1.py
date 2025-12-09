5c2d65c4958723dde21cf4cda737147eaed534c2
kunal <kp961704@gmail.com>
2025-12-09 14:17:25 +0530

    excel support added

---
+++ main1.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Dummy in-memory database
+users_db = [
+    {"id": 1, "name": "Rahul", "age": 25},
+    {"id": 2, "name": "Aisha", "age": 22},
+]
+
+# Request body ke liye model
+class UserCreate(BaseModel):
+    name: str
+    age: int
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Kunal's API running successfully!"}
+
+
+# 1) Saare users laane ka endpoint
+@app.get("/users")
+def get_users():
+    return {"users": users_db}
+
+
+# 2) Specific user by id
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    for user in users_db:
+        if user["id"] == user_id:
+            return user
+    return {"error": "User not found"}
+
+
+# 3) Naya user create karne ka endpoint (POST)
+@app.post("/users")
+def create_user(user: UserCreate):
+    new_id = len(users_db) + 1
+    new_user = {"id": new_id, "name": user.name, "age": user.age}
+    users_db.append(new_user)
+    return new_user
